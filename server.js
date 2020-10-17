bb77ddc3d3d8d54d3d0a09c3bf0f6b4abe4bc519
Brennan Doehring <nannerbdale@gmail.com>
2020-10-17 06:43:33 -0400

    Added routes and post.

--- server.js
+++ server.js
@@ -1,16 +1,44 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const bodyParser = require('body-parser');
+const uuidv1 = require('uuid/v1');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // GET Request
 app.get('/api/notes', (req, res) => {
     fs.readFile('db/db.json', 'utf8', function(err, contents) {
-        let words = JSON.parse(contents);
-        res.send(words);
+        let notes = JSON.parse(contents);
+        res.send(notes);
     });
 });
 
+// POST Request
+app.post('/api/notes', (req, res) => {
+    fs.readFile('db/db.json', (err, data) => {
+        if (err) throw err;
+        let json = JSON.parse(data);
+        let userNote = {
+            title: req.body.title,
+            text: req.body.text,
+            id: uuidv1()
+        }
+        json.push(userNote);
+    });
+});
 
+// ROUTES
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+  
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+// LISTEN on PORT 3000
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
+});
