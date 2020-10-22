2e03979acf3e79ae073e78b5c86b3185f1addae0
Brennan Doehring <brennandoehringd@outlook.com>
2020-10-22 19:33:57 -0400

    Application fully functioning.

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const bodyParser = require('body-parser');
-const uuidv1 = require('uuid/v1');
+const { v4: uuidv1 } = require('uuid');
+const { urlencoded } = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+//set up body parsing and static files
+app.use(express.json());
+app.use(urlencoded({extended: true}));
+app.use(express.static("public"));
+
 
 // GET Request
 app.get('/api/notes', (req, res) => {
@@ -17,28 +24,45 @@ app.get('/api/notes', (req, res) => {
 
 // POST Request
 app.post('/api/notes', (req, res) => {
-    fs.readFile('db/db.json', (err, data) => {
+    
+
+    fs.readFile('db/db.json', "utf8", (err, data) => {
         if (err) throw err;
-        let json = JSON.parse(data);
+           let arr = JSON.parse(data)
+           
         let userNote = {
             title: req.body.title,
             text: req.body.text,
             id: uuidv1()
         }
-        json.push(userNote);
-    });
+         arr.push(userNote);
+         fs.writeFileSync("db/db.json", JSON.stringify(arr));
+         res.json(userNote);
+    });  
 });
+
+
+app.delete("/api/notes/:id", (req, res) => {
+    console.log(req.params.id)
+    fs.readFile('db/db.json', "utf8", (err, data) => {
+        if (err) throw err;
+        let arr = JSON.parse(data)
+        let filteredNotes = arr.filter((note) => note.id !== req.params.id )
+        fs.writeFileSync("db/db.json", JSON.stringify(filteredNotes));
+        res.json({ok:true})
+    }); 
+})
 
 // ROUTES
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
   
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 // LISTEN on PORT 3000
-app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`)
 });
